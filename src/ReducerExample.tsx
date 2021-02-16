5fbb3853e61c21611b95ceac2d78f41d79f96855
brockbyrd <brockbyrdd@gmail.com>
2021-02-16 14:17:57 -0600

    sets up todo reducer

---
+++ src/ReducerExample.tsx
@@ -0,0 +1,23 @@
+import React, { useReducer } from 'react';
+
+type Action =
+    | { type: "add"; text: string }
+    | { type: "remove"; index: number;};
+
+interface Todo {
+    text: string;
+    complete: boolean;
+}
+
+type State = Todo[];
+
+const TodoReducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case "add":
+            return [...state, { text: action.text, complete: false }];
+        case "remove":
+            return state.filter((_, i) => action.index !== i);
+        default:
+            return state;
+    }
+}
